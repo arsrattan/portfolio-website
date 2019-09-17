6783f099c8671a0552ae49c3831b5f984563baa6
Anmol Rattan <anmolrattan@Anmols-iMac.local>
2019-09-17 17:49:32 -0400

    Added social media buttons

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -3,9 +3,15 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return (
-            <div className='footerContainer'>
+            <div className='container footer-container'>
             <footer>
-                Footer text
+                <div className='social-media-container'>
+                    <a href='https://www.facebook.com/anmol.rattan.9' className='fab fa-facebook fa-3x' target="_blank"><span>Link to Anmol's Facebook page.</span></a>
+                    <a href='https://www.instagram.com/_anmolrattan_/' className='fab fa-instagram fa-3x' target="_blank"><span>Link to Anmol's Instagram page.</span></a>
+                    <a href='https://www.linkedin.com/in/anmolrattan23/' className='fab fa-linkedin-in fa-3x' target="_blank"><span>Link to Anmol's LinkedIn page.</span></a>
+                    <a href='https://github.com/arsrattan' className='fab fa-github fa-3x' target="_blank"><span>Link to Anmol's GitHub page.</span></a>
+                    <a href='https://www.hackerrank.com/arsrattan' className='fab fa-hackerrank fa-3x' target="_blank"><span>Link to Anmol's HackerRank page.</span></a>
+                </div>
             </footer>
             </div>
         )
